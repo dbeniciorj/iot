7337ce3e4ec392617bb8a4594b712ae5ec5f111a
root <contato@dbenicio.com>
2018-11-19 14:33:45 -0200

    upd shellscript

--- sensor.sh
+++ sensor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 BTMAC="24:0A:C4:9A:E8:EA"
+DB=iot
+DBPWD=1
 READ="$(gatttool -b $BTMAC --char-read --handle=0x002a | cut -d : -f2| xxd -r -p)"
 echo READ= $READ
 # exit
@@ -7,3 +9,4 @@ TEMP=$(echo $READ|cut -d "," -f1)
 HUMD=$(echo $READ|cut -d "," -f2)
 echo TEMP= $TEMP
 echo HUMD= $HUMD
+mysql $DB -p$DBPWD -e "insert into tempLog values (now(),$TEMP,$HUMD)"
